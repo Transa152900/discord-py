497b542b04cec71aab6641014fd3b0679ea2b37d
Transa152900 <76061884+Transa152900@users.noreply.github.com>
2022-06-23 00:20:10 +0500

    Update main.py

--- main.py
+++ main.py
@@ -1,26 +1,23 @@
 # This example requires the 'message_content' privileged intents
 
 import os
-import discord
-from discord.ext import commands
+import disnake
+from disnake.ext import commands
 
-
-intents = discord.Intents.default()
-intents.message_content = True
-bot = commands.Bot(command_prefix='!', intents=intents)
-
+bot=commands.Bot(command_prefix='T+', intents=disnake.Intents.all())
 
 @bot.event
 async def on_ready():
     print(f"Logged in as {bot.user}")
 
 @bot.command()
-async def ping(ctx):
-    await ctx.send('pong')
+async def say_t(ctx, text, title):
+    emb=disnake.Embed(description=text, title=title)
+    await ctx.send(embed=emb)
 
 @bot.command()
-async def hello(ctx):
-    await ctx.send("Choo choo! 🚅")
-
+async def say(ctx, text):
+    emb=disnake.Embed(description=text)
+    await ctx.send(embed=emb)
 
 bot.run(os.environ["DISCORD_TOKEN"])
